6ade8b3f6d0ea4478f8aa280b3e5bb2b5b38525d
Kevin Evans <kwevans@gmail.com>
2020-06-05 17:02:20 +0100

    try two

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
+set -eu
 
-set -e
+if [ -z "$INPUT_HOST" ]; then
+    echo "Input remote_host is required!"
+    exit 1
+fi
 
-SSH_PATH="$HOME/.ssh"
+# Extra handling for SSH-based connections.
+if [ ${INPUT_HOST#"ssh://"} != "$INPUT_HOST" ]; then
+    SSH_HOST=${INPUT_HOST#"ssh://"}
+    SSH_HOST=${SSH_HOST#*@}
 
-echo "Registering SSH keys ..."
-mkdir "$SSH_PATH"
-touch "$SSH_PATH/known_hosts"
+    if [ -z "$INPUT_PRIVATE_KEY" ]; then
+        echo "Input ssh_private_key is required for SSH hosts!"
+        exit 1
+    fi
 
-echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
-echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
+    if [ -z "$INPUT_PUBLIC_KEY" ]; then
+        echo "Input ssh_public_key is required for SSH hosts!"
+        exit 1
+    fi
 
-chmod 700 "$SSH_PATH"
-chmod 600 "$SSH_PATH/known_hosts"
-chmod 600 "$SSH_PATH/deploy_key"
-chmod 600 "$SSH_PATH/deploy_key.pub"
+    echo "Registering SSH keys..."
 
-eval $(ssh-agent)
-ssh-add "$SSH_PATH/deploy_key"
+    # Save private key to a file and register it with the agent.
+    mkdir -p "$HOME/.ssh"
+    printf '%s' "$INPUT_PRIVATE_KEY" > "$HOME/.ssh/docker"
+    chmod 600 "$HOME/.ssh/docker"
+    eval $(ssh-agent)
+    ssh-add "$HOME/.ssh/docker"
 
-ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
+    # Add public key to known hosts.
+    printf '%s %s\n' "$SSH_HOST" "$INPUT_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
+fi
 
-echo "Connection to host ..."
-ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $USER@$HOST "$*"
+echo "Connecting to $INPUT_HOST..."
+docker --log-level debug --host "$INPUT_HOST" "$@" 2>&1
