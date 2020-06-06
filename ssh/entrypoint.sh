1ee1318ab772060185059d9819016e246e700f13
Kevin Evans <kwevans@gmail.com>
2020-06-06 10:55:33 +0100

    Update entrypoint.sh

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -7,8 +7,8 @@ SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
 touch "$SSH_PATH/known_hosts"
 
-echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
-echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
+echo "$INPUT_PRIVATE_KEY" > "$SSH_PATH/deploy_key"
+echo "$INPUT_PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
 
 chmod 700 "$SSH_PATH"
 chmod 600 "$SSH_PATH/known_hosts"
@@ -18,6 +18,6 @@ chmod 600 "$SSH_PATH/deploy_key.pub"
 eval $(ssh-agent)
 ssh-add "$SSH_PATH/deploy_key"
 
-ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
+ssh-keyscan -t rsa $INPUT_HOST >> "$SSH_PATH/known_hosts"
 
-ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $USER@$HOST "$*"
+ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $INPUT_USER@$HOST "$*"
