c235fecb08e4f3622b7b519429ec48d00afff57e
Kevin Evans <kwevans@gmail.com>
2020-06-06 10:48:28 +0100

    going back

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -1,38 +1,23 @@
 #!/bin/sh
-set -eu
 
-if [ -z "$INPUT_HOST" ]; then
-    echo "Input remote_host is required!"
-    exit 1
-fi
+set -e
 
-# Extra handling for SSH-based connections.
-if [ ${INPUT_HOST#"ssh://"} != "$INPUT_HOST" ]; then
-    SSH_HOST=${INPUT_HOST#"ssh://"}
-    SSH_HOST=${SSH_HOST#*@}
+SSH_PATH="$HOME/.ssh"
 
-    if [ -z "$INPUT_PRIVATE_KEY" ]; then
-        echo "Input ssh_private_key is required for SSH hosts!"
-        exit 1
-    fi
+mkdir "$SSH_PATH"
+touch "$SSH_PATH/known_hosts"
 
-    if [ -z "$INPUT_PUBLIC_KEY" ]; then
-        echo "Input ssh_public_key is required for SSH hosts!"
-        exit 1
-    fi
+echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
+echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
 
-    echo "Registering SSH keys..."
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/known_hosts"
+chmod 600 "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key.pub"
 
-    # Save private key to a file and register it with the agent.
-    mkdir -p "$HOME/.ssh"
-    printf '%s' "$INPUT_PRIVATE_KEY" > "$HOME/.ssh"
-    chmod 600 "$HOME/.ssh"
-    eval $(ssh-agent)
-    ssh-add "$HOME/.ssh"
+eval $(ssh-agent)
+ssh-add "$SSH_PATH/deploy_key"
 
-    # Add public key to known hosts.
-    printf '%s %s\n' "$SSH_HOST" "$INPUT_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
-fi
+ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
 
-echo "Connecting to $INPUT_HOST..."
-ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $INPUT_USER@$INPUT_HOST "$*"
+ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $USER@$HOST "$*"
