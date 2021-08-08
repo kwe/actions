d8bb7382937ad284ef979eaf6f22d33d7fe99474
Kevin Evans <kwevans@gmail.com>
2021-08-08 01:36:20 +0100

    going back

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -20,6 +20,6 @@ ssh-add "$SSH_PATH/deploy_key"
 
 ssh-keyscan -t rsa $INPUT_HOST >> "$SSH_PATH/known_hosts"
 
-echo "$INPUT_PORT"
+echo "${INPUT_PORT:-22} $INPUT_USER@$INPUT_HOST"
 
-ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${INPUT_PORT} ${INPUT_USER}@${INPUT_HOST} "$*"
+ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${INPUT_PORT:-22} $INPUT_USER@$INPUT_HOST "$*"
